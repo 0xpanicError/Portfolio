d19e4c336dc23718f5b16c3a19d3e8484c794b0b
0xpanicError <theyashsaraswat@gmail.com>
2024-01-13 19:24:56 +0530

    update App State using POST request

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,34 +1,57 @@
 use actix_web::{web, App, HttpServer};
+use serde::{Deserialize, Serialize};
+use std::clone::Clone;
 use std::sync::Mutex;
 
+#[derive(Clone, Debug)]
 pub struct ArticleScheme {
     id: i32,
     title: String,
 }
 
+#[derive(Deserialize, Serialize)]
+pub struct NewArticle {
+    title: String,
+}
+
 pub struct AppState {
-    pub articles: Mutex<ArticleScheme>,
+    pub articles: Mutex<Vec<ArticleScheme>>,
 }
 
 async fn index(data: web::Data<AppState>) -> String {
-    let mut article = data.articles.lock().unwrap();
-    article.id += 1;
-    format!("Article ID: {}, Title: {}", article.id, article.title)
+    let mut articles = data.articles.lock().unwrap();
+
+    let new_article = ArticleScheme {
+        id: (articles.len() + 1) as i32,
+        title: format!("Article {}", articles.len() + 1),
+    };
+
+    articles.push(new_article.clone());
+    format!("Articles: {:?}", articles)
+}
+
+async fn create_article(new_article: web::Json<NewArticle>, data: web::Data<AppState>) -> String {
+    let mut articles = data.articles.lock().unwrap();
+
+    let new_article = ArticleScheme {
+        id: (articles.len() + 1) as i32,
+        title: new_article.title.clone(),
+    };
+    articles.push(new_article);
+    format!("Articles: {:?}", articles)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let article = web::Data::new(AppState {
-        articles: Mutex::new(ArticleScheme {
-            id: 0,
-            title: "Hello World".to_string(),
-        }),
+    let app_state = web::Data::new(AppState {
+        articles: Mutex::new(Vec::new()),
     });
 
     HttpServer::new(move || {
         App::new()
-            .app_data(article.clone())
+            .app_data(app_state.clone())
             .route("/", web::get().to(index))
+            .route("/create", web::post().to(create_article))
     })
     .bind("127.0.0.1:8080")?
     .run()
