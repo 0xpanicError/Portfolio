31f23daa139dbe027d9dd8ac44762ed472675b2f
0xpanicError <theyashsaraswat@gmail.com>
2024-01-13 23:47:18 +0530

    get article by id

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,9 +1,9 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use std::clone::Clone;
 use std::sync::Mutex;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize)]
 pub struct ArticleScheme {
     id: i32,
     title: String,
@@ -18,27 +18,41 @@ pub struct AppState {
     pub articles: Mutex<Vec<ArticleScheme>>,
 }
 
-async fn index(data: web::Data<AppState>) -> String {
-    let mut articles = data.articles.lock().unwrap();
-
-    let new_article = ArticleScheme {
-        id: (articles.len() + 1) as i32,
-        title: format!("Article {}", articles.len() + 1),
-    };
-
-    articles.push(new_article.clone());
-    format!("Articles: {:?}", articles)
+async fn get_article(data: web::Data<AppState>) -> impl Responder {
+    let articles = data.articles.lock().unwrap();
+    println!("Getting articles{:?}", articles);
+    return HttpResponse::Ok().json(&*articles);
 }
 
-async fn create_article(new_article: web::Json<NewArticle>, data: web::Data<AppState>) -> String {
-    let mut articles = data.articles.lock().unwrap();
+// async fn get_article_by_id(
+//     article_id: web::Path<i32>,
+//     data: web::Data<AppState>,
+// ) -> impl Responder {
+//     let articles = data.articles.lock().unwrap();
+//     let article = articles
+//         .iter()
+//         .find(|&article| article.id == *article_id)
+//         .unwrap();
+//     println!("Getting article{:?}", article);
+//     return HttpResponse::Ok().json(&article);
+// }
 
-    let new_article = ArticleScheme {
-        id: (articles.len() + 1) as i32,
-        title: new_article.title.clone(),
-    };
-    articles.push(new_article);
-    format!("Articles: {:?}", articles)
+async fn create_article(
+    new_article: web::Json<NewArticle>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    {
+        let mut articles = data.articles.lock().unwrap();
+
+        let new_article = ArticleScheme {
+            id: (articles.len() + 1) as i32,
+            title: new_article.title.clone(),
+        };
+        articles.push(new_article);
+    }
+    let articles = data.articles.lock().unwrap();
+    println!("Creating articles{:?}", articles);
+    return HttpResponse::Ok().json(&*articles);
 }
 
 #[actix_web::main]
@@ -48,10 +62,11 @@ async fn main() -> std::io::Result<()> {
     });
 
     HttpServer::new(move || {
-        App::new()
-            .app_data(app_state.clone())
-            .route("/", web::get().to(index))
-            .route("/create", web::post().to(create_article))
+        App::new().app_data(app_state.clone()).service(
+            web::scope("/articles")
+                .route("/", web::get().to(get_article))
+                .route("/create", web::post().to(create_article)),
+        )
     })
     .bind("127.0.0.1:8080")?
     .run()
