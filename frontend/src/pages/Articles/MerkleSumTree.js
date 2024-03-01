e1408bcdf660276ede41e704c5ae3c64f5015772
0xpanicError <theyashsaraswat@gmail.com>
2024-03-01 23:13:24 +0530

    MST update

--- frontend/src/pages/Articles/MerkleSumTree.js
+++ frontend/src/pages/Articles/MerkleSumTree.js
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar";
 import styles from "../../styles/Page.module.css";
-import ArticleCover from "../../assets/MST_1.png";
+import ArticleCover from "../../assets/MST_cover.png";
+import Img1 from "../../assets/MST_1.png";
 import Img2 from "../../assets/MST_2.png";
 import Img3 from "../../assets/MST_3.png";
 
@@ -24,17 +25,60 @@ const MerkleSumTreeArticle = () => {
             className={styles.articleImage}
           />
           <br />
+          <h3>Introduction</h3>
+          Since the introduction of Bitcoin, people have been interested in
+          investing and trading in cryptocurrencies. As a result of that,
+          countless exchanges arose in the 2010s that can facilitate this
+          transaction. But unlike stock exchanges, there were no regulatory
+          bodies like SEC, SEBI or ESMA. Hence the only option you have in
+          obtaining cryptocurrencies is to trust the exchange and hope they have
+          your reserves.
           <br />
-          A Merkle Sum Tree is similar to a Merkle Tree but in addition to
-          hashes, it also has a balance value for each node. Each leaf node
-          contains the hash of a username and a balance of that user. The
-          balance of each middle node is the sum of the balances of its child
-          nodes and hence the root of this Merkle Sum Tree has the cumulative
-          balance of all its leaf nodes. This scheme was introduced for
-          custodians to commit proof of liabilities so that there is
-          cryptographic evidence of solvency. Custodians are supposed to include
-          balances of each user in the leaf nodes and build a tree to report
-          their total liabilities.
+          <br />
+          This was quite obviously the opposite dynamic of what Bitcoin
+          promotes, which was a trustless world where code is law and everything
+          is mathematically guaranteed. This led to the creation of Proof of
+          Solvency solutions, a cryptographic way to prove that an exchange
+          holds more assets than the liabilities due to its users. This would
+          ensure that in case of bank run, the exchange would be successful in
+          satisfying the withdrawals of each and every user.
+          <br />
+          <h3>Proof of Solvency</h3>
+          Proof of Solvency can be achieved by having a commitment of assets and
+          liabilities on chain. Proving that the exchange holds some assets is
+          quite easy, you just need a signature to prove that an exchange does
+          indeed own a bunch of accounts who’s balance can easily be verified on
+          the blockchain. But proving how much liability the exchange has is a
+          bit tricky. This is because the exchange receives real world assets
+          from the user and holds their cryptocurrency on their behalf. This is
+          why these exchanges are also called custodians. Decentralized
+          exchanges like Uniswap don’t face this problem as the users have
+          control over their assets and the exchange happens through smart
+          contracts on chain.
+          <br />
+          <br />
+          For example, Summa is a zero knowledge proof of solvency protocol
+          developed by Privacy and Scaling Ethereum. They let custodians
+          generate proof of solvency while keeping their sensitive data private.
+          According to Summa's requirements, the equation must be satisfied
+          between assets and liabilities for each cryptocurrency to generate a
+          valid Proof of Solvency.
+          <br />
+          <h3>Merkle Sum Tree</h3>
+          <img
+            src={Img1}
+            alt="Merkle Sum Tree"
+            className={styles.articleImage}
+          />
+          <br />
+          <br />
+          To prove a commitment of liabilities, the Merkle Sum Tree (also called
+          a Maxwell Tree) was introduced. A Merkle Sum Tree is similar to a
+          Merkle Tree but in addition to the hash of the contents, it also has a
+          balance value for each leaf node. Each leaf node contains the hash of
+          a username and a balance of that user. The balance of each middle node
+          is the sum of the balances of its child nodes and hence the root of
+          this Merkle Sum Tree has the cumulative balance of all its leaf nodes.
           <br />
           <br />
           The leaf node includes information like the balance, username or Id
